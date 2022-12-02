0089bd824a325d101e68866826d6db152c90e9be
Maciek Makowski <maciek@mmakowski.com>
2022-12-03 00:40:59 +0100

    1b done

--- aoc-01/src/main.rs
+++ aoc-01/src/main.rs
@@ -2,25 +2,23 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
-    let mut max: u64 = 0;
+    let mut totals: Vec<u64> = Vec::new();
     let mut curr: u64 = 0;
     let file = File::open("input.txt")?;
     for line in io::BufReader::new(file).lines() {
         if let Ok(l) = line {
             if l.is_empty() {
-                println!("{}", curr);
-                if curr > max {
-                    max = curr;
-                }
+                totals.push(curr);
                 curr = 0;
             } else {
                 curr += l.parse::<u64>().unwrap();
             }
         }
     }
-    if curr > max {
-        max = curr;
-    }
-    println!("max: {}", max);
+    totals.push(curr);
+    totals.sort();
+    totals.reverse();
+    println!("max: {}", totals[0]);
+    println!("top 3 total: {}", totals[0] + totals[1] + totals[2]);
     return Ok(());
 }
