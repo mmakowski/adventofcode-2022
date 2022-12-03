51ab189fab73973354c340e20a36953668680b09
Maciek Makowski <maciek@mmakowski.com>
2022-12-03 21:30:32 +0100

    refactor; day 2 b

--- src/lib.rs
+++ src/lib.rs
@@ -64,10 +64,14 @@ mod day_1 {
 mod day_2 {
     use std::fs::File;
     use std::io::{self, BufRead};
+    use std::str::FromStr;
 
     mod rps {
+        #[derive(Clone, Copy)]
         pub enum Move {
-            Rock, Paper, Scissors
+            Rock,
+            Paper,
+            Scissors,
         }
 
         pub fn my_score(my_move: Move, their_move: Move) -> u64 {
@@ -92,40 +96,108 @@ mod day_2 {
         }
     }
 
-    fn decode(code: &str) -> Result<rps::Move, &str> {
-        use rps::Move::*;
-        match code {
-            "A" => Ok(Rock),
-            "B" => Ok(Paper),
-            "C" => Ok(Scissors),
-            "X" => Ok(Rock),
-            "Y" => Ok(Paper),
-            "Z" => Ok(Scissors),
-            _ => Err(code),
+    use rps::Move::*;
+
+    impl FromStr for rps::Move {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "A" => Ok(Rock),
+                "B" => Ok(Paper),
+                "C" => Ok(Scissors),
+                "X" => Ok(Rock),
+                "Y" => Ok(Paper),
+                "Z" => Ok(Scissors),
+                _ => Err(String::from(s)),
+            }
         }
     }
 
-    fn score() -> io::Result<u64> {
+    fn score_with_assumption() -> io::Result<u64> {
         let file = File::open("input-02.txt")?;
         let mut total: u64 = 0;
         for line in io::BufReader::new(file).lines() {
             if let Ok(l) = line {
                 let mut parts = l.split_whitespace();
-                let their_move = parts.next().unwrap();
-                let my_move = parts.next().unwrap();
-                total += rps::my_score(decode(my_move).unwrap(), decode(their_move).unwrap());
+                let mut next_move = || { parts.next().unwrap().parse::<rps::Move>().unwrap() };
+                let their_move = next_move();
+                let my_move = next_move();
+                total += rps::my_score(my_move, their_move);
             }
         }
         Ok(total)
     }
+
+    enum RoundResult {
+        Win,
+        Draw,
+        Lose,
+    }
+
+    impl FromStr for RoundResult {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            use RoundResult::*;
+            match s {
+                "X" => Ok(Lose),
+                "Y" => Ok(Draw),
+                "Z" => Ok(Win),
+                _ => Err(String::from(s))
+            }
+        }
+    }
+
+    fn choose_move(their_move: rps::Move, intended_result: RoundResult) -> rps::Move {
+        use RoundResult::*;
+        match their_move {
+            Rock => match intended_result {
+                Win => Paper,
+                Draw => Rock,
+                Lose => Scissors,
+            },
+            Paper => match intended_result {
+                Win => Scissors,
+                Draw => Paper,
+                Lose => Rock,
+            },
+            Scissors => match intended_result {
+                Win => Rock,
+                Draw => Scissors,
+                Lose => Paper,
+            },
+        }
+    }
+
+    fn score_with_full_info() -> io::Result<u64> {
+        let file = File::open("input-02.txt")?;
+        let mut total: u64 = 0;
+        for line in io::BufReader::new(file).lines() {
+            if let Ok(l) = line {
+                let mut parts = l.split_whitespace();
+                let their_move = parts.next().unwrap().parse::<rps::Move>().unwrap();
+                let intended_result = parts.next().unwrap().parse::<RoundResult>().unwrap();
+                let my_move = choose_move(their_move, intended_result);
+                total += rps::my_score(my_move, their_move);
+            }
+        }
+        Ok(total)
+    }
+
 
     #[cfg(test)]
     mod run {
         use super::*;
 
         #[test]
-        fn print_score() {
-            println!("{}", score().unwrap());
+        fn print_score_with_assumption() {
+            println!("{}", score_with_assumption().unwrap());
+        }
+
+        #[test]
+        fn print_score_with_full_info() {
+            println!("{}", score_with_full_info().unwrap());
         }
     }
 }
