199a7b302f5687c5e127768136d0ec6bef367ece
Maciek Makowski <maciek@mmakowski.com>
2022-12-06 13:07:20 +0100

    day 6 b

--- src/day_06.rs
+++ src/day_06.rs
@@ -10,16 +10,23 @@ enum Error {
     MarkerNotFound,
 }
 
-fn prefix_length() -> Result<usize, Error> {
-    const MARKER_LENGTH: usize = 4;
+fn prefix_length(marker_length: usize) -> Result<usize, Error> {
     let stream = read_to_string("input-06.txt").map_err(|e| IO(e))?;
-    for i in MARKER_LENGTH..stream.len() {
-        let set: HashSet<char> = HashSet::from_iter(stream[i - MARKER_LENGTH..i].chars());
-        if set.len() == 4 {
+    for i in marker_length..stream.len() {
+        let set: HashSet<char> = HashSet::from_iter(stream[i - marker_length..i].chars());
+        if set.len() == marker_length {
             return Ok(i);
         }
     }
     Err(MarkerNotFound)
+}
+
+fn packet_prefix_length() -> Result<usize, Error> {
+    prefix_length(4)
+}
+
+fn message_prefix_length() -> Result<usize, Error> {
+    prefix_length(14)
 }
 
 #[cfg(test)]
@@ -27,8 +34,13 @@ mod run {
     use super::*;
 
     #[test]
-    fn print_prefix_length() {
-        println!("{}", prefix_length().unwrap());
+    fn print_packet_prefix_length() {
+        println!("{}", packet_prefix_length().unwrap());
+    }
+
+    #[test]
+    fn print_message_prefix_length() {
+        println!("{}", message_prefix_length().unwrap());
     }
 }
 
